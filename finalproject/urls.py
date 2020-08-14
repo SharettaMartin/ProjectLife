b873ce5cf103d178acc1ebf0c67f1677657ce30c
tantoine04 <tantoine04@gmail.com>
2020-08-14 03:47:53 +0000

    bug fixes

--- finalproject/urls.py
+++ finalproject/urls.py
@@ -1,5 +1,3 @@
-
-
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
@@ -11,15 +9,24 @@ urlpatterns = [
     path('accounts/', include('registration.backends.simple.urls')),
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('profile/', views.profile),
-    path('detail/<int:pk>', views.patient_detail),
     path('patient/', views.patient, name='patient'),
     #path('public_profile/<int:pk>', views.profile, name='dashboard'),
     #path('detail/<int:pk>', views.patient_detail, name='patient_dashboard'),
     path('donor/new', views.new_donor, name='new_donor'),
     path('instructions/', views.instructions, name='instructions'),
+
+    #profile to dashboard Name
+    
     path('dashboard/<int:pk>', views.dashboard, name='patient_dashboard'),
+
+    #detail to profile name
     path('profile/<int:pk>', views.profile, name='profile'),
+
     path('responses/', views.responses, name='responses'),
 
-   ]
+    #path('list_patients/', views.list_patients, name='list_patients')
+
+    # path('myapp/donor', myapp_views.donor_views, name='donor_views'),
+    # path('myapp/add/', myapp_views.add_patient, name='add_patient'),
+    # path('myapp.')
+] 
